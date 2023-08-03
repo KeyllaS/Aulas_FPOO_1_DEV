fcd8512e2dfdf8be1634cad10a8ebd37fa579480
keyllasilva@aluno.senai.br <sarah.silva5@aluno.senai.br>
2023-08-03 13:54:49 -0300

    testando codigo

---
+++ Fundamentos/src/atividade01/AprovacaoEmprestimo.java
@@ -0,0 +1,11 @@
+package atividade01;
+
+import java.util.Scanner;
+
+public class AprovacaoEmprestimo {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.i);
+        
+    }
+}
--- Fundamentos/src/atividade01/CalculoPassagem.java
+++ Fundamentos/src/atividade01/CalculoPassagem.java
@@ -1,5 +1,23 @@
 package atividade01;
 
-public class CalculoPassagem {
+import java.util.Scanner;
 
+public class CalculoPassagem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite a distancia em km que deseja percorrer: ");
+        double distancia = scanner.nextDouble();
+        
+        double precoPorKm;
+        if (distancia <=200)
+            precoPorKm = 0.50;
+    } else {
+        precoPorKm = 0.50;
+    }
+    
+    double calculoPassagem = distancia * precoPorKm;
+    
+    System.ou
 }
+
